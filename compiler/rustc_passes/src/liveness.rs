ed9fe26a02a7eba820f350e3827afa87ddd9d4e2
Cormac Relf <web@cormacrelf.net>
2021-10-13 16:39:06 +1100

    intro hir::LetExpr and type check it like a hir::Local

--- compiler/rustc_passes/src/liveness.rs
+++ compiler/rustc_passes/src/liveness.rs
@@ -429,7 +429,7 @@ impl<'tcx> Visitor<'tcx> for IrMaps<'tcx> {
                 intravisit::walk_expr(self, expr);
             }
 
-            hir::ExprKind::Let(ref pat, ..) => {
+            hir::ExprKind::Let(hir::LetExpr { pat, .. }) => {
                 self.add_from_pat(pat);
                 intravisit::walk_expr(self, expr);
             }
@@ -856,7 +856,7 @@ impl<'a, 'tcx> Liveness<'a, 'tcx> {
                 })
             }
 
-            hir::ExprKind::Let(ref pat, ref scrutinee, _) => {
+            hir::ExprKind::Let(hir::LetExpr { pat, scrutinee, .. }) => {
                 let succ = self.propagate_through_expr(scrutinee, succ);
                 self.define_bindings_in_pat(pat, succ)
             }
@@ -1401,7 +1401,7 @@ fn check_expr<'tcx>(this: &mut Liveness<'_, 'tcx>, expr: &'tcx Expr<'tcx>) {
             }
         }
 
-        hir::ExprKind::Let(ref pat, ..) => {
+        hir::ExprKind::Let(hir::LetExpr { pat, .. }) => {
             this.check_unused_vars_in_pat(pat, None, |_, _, _, _| {});
         }
 
