153b17396029e0c743908d18701088706f6c8cc3
Cormac Relf <web@cormacrelf.net>
2021-10-17 16:42:22 +1100

    add a bunch of tests adapted from rfc2005 and elsewhere

---
+++ src/test/ui/let-else/let-else-binding-explicit-mut-annotated.rs
@@ -0,0 +1,20 @@
+// from rfc2005 test suite
+
+#![feature(let_else)]
+
+// Verify the binding mode shifts - only when no `&` are auto-dereferenced is the
+// final default binding mode mutable.
+
+fn main() {
+    let Some(n): &mut Option<i32> = &&Some(5i32) else { return }; //~ ERROR mismatched types
+    *n += 1;
+    let _ = n;
+
+    let Some(n): &mut Option<i32> = &&mut Some(5i32) else { return }; //~ ERROR mismatched types
+    *n += 1;
+    let _ = n;
+
+    let Some(n): &mut Option<i32> = &mut &mut Some(5i32) else { return }; // OK
+    *n += 1;
+    let _ = n;
+}
