642f93b3c423d39d605212598906641c3c37c250
Cormac Relf <web@cormacrelf.net>
2021-12-13 00:06:48 +1100

    build out the ref/ref mut tests for let-else

--- src/test/ui/let-else/issue-89960.rs
+++
@@ -1,9 +0,0 @@
-// check-pass
-// This had an ICE, see issue #89960
-
-#![feature(let_else)]
-
-fn main() {
-    // (You can't use just `_` in any `ref`/`ref mut` pattern)
-    let Some(ref mut _a) = Some(()) else { return };
-}
